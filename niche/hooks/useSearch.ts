78f3ad10e63eaea4eebd30fcf32147f1a88c83dc
Christopher Lenhardt <christopher.e.lenhardt@gmail.com>
2026-02-22 03:37:44 -0500

    Add CORS

---
+++ niche/hooks/useSearch.ts
@@ -0,0 +1,42 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+export function useSearch(route: string) {
+  const [response, setResponse] = useState<unknown | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    if (!route) return;
+    setLoading(true);
+    console.log('loading should be true:', loading)
+    setResponse(null);
+    setError(null);
+    const backend_adrs = process.env.NEXT_PUBLIC_BACKEND_ADDRESS;
+
+    async function fetchData() {
+      try {
+        const res = await fetch(`${backend_adrs}/${route}`, { next: { revalidate: 3600 } });
+        if (!res.ok) {
+          throw new Error(`Request failed with status ${res.status}`);
+        }
+        const [data] = await Promise.all([
+        res.json(),
+        new Promise(resolve => setTimeout(resolve, 1000))
+        ]);
+        setResponse(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err instanceof Error ? err.message : 'An unknown error occurred');
+      } finally {
+        setLoading(false);
+        console.log(loading)
+      }
+    }
+
+    fetchData()
+  }, [route])
+  
+  return { response, loading, error }
+}
